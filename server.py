8b9814ed969dad8a03db3920d90789696442cccf
hyerim1048 <gpflav@gmail.com>
2020-03-01 18:05:43 +0900

    test for microphone data

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from aiohttp import web 
 import socketio 
+import io
+import scipy.io.wavfile as wavf
+import numpy as np
 
 # Async Socket IO Server 
 sio = socketio.AsyncServer() 
@@ -12,17 +15,20 @@ async def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
     
-    mic 
     
-    
-@sio.on('message')
-async def print_message(sid, message):
+
+@sio.on('mic')
+async def print_message(sid, *data):
     # a new event of type message through socket.io
     # socket ID and the message 
     print("Socket ID: ", sid)
-    print(message)
-    
-    await sio.emit('message', message[::-1])
+    print(len(data), type(data))
+
+    fs = 44100
+    out_f = '/wav_files/out.wav'
+
+    wavf.write(out_f, fs, np.array(data))
+    await sio.emit('mic', "completed")
 
   
 # router
