1ff04c8831bb4a8bfb783205f2ac0ddf46e68bc9
hyerim1048 <gpflav@gmail.com>
2020-03-01 19:51:42 +0900

    add monkey's mfcc

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.7 
 ENV PYTHONUNBUFFERED=1
+RUN apt-get update && apt-get install -y libsndfile1
 WORKDIR /
 COPY ./requirements.txt /
 RUN pip install -r /requirements.txt
+
 COPY . /
+
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socketio
 import io
 import scipy.io.wavfile as wavf
 import numpy as np
+from preprocess.MFCC_generator import MFCC_generator
 
 # Async Socket IO Server 
 sio = socketio.AsyncServer() 
@@ -16,22 +17,32 @@ async def index(request):
         return web.Response(text=f.read(), content_type='text/html')
     
 global_data = []
-
+TMP_DIR = '/wav_files'
 @sio.on('mic')
 async def print_message(sid, *data):
-    # a new event of type message through socket.io
-    # socket ID and the message 
+
     print("Socket ID: ", sid)
     print(len(data), type(data))
 
     fs = 44100
-    out_f = '/wav_files/out.wav'
-    global_data.extend(list(data))
-    print(len(global_data))
-    wavf.write(out_f, fs, np.array(global_data, dtype=np.int16))
-    await sio.emit('mic', "completed")
+    filename = f"{TMP_DIR}/{sid}.wav"
+    try: 
+        prev_wav = wavf.read(filename, mmap=False)
+        prev_data = list(prev_wav[1])
+        print(type(prev_data),len(prev_data))
+        prev_data.extend(list(data))
+        print(len(prev_data))
+        wavf.write(filename, fs, np.array(prev_data, dtype=np.int16))
 
+        generator = MFCC_generator()
+        arr = generator.get_wav_mfcc(filename, start=0, duration=10)
+        np.save("/wav_files/sample_mfcc.npy", arr)
+        await sio.emit('mic', "completed")
   
+    except OSError as e: 
+        wavf.write(filename, fs, np.array(data, dtype=np.int16))
+
+
 # router
 app.router.add_get('/', index)
 
